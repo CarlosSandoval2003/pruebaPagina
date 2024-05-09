2a41ddc84ad4abd25ea3f21961edb00f5d4455e6
CarlosSandoval2003 <98406391+CarlosSandoval2003@users.noreply.github.com>
2024-05-09 14:59:23 -0600

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   useEffect(() => {
     const fetchParentCategories = async () => {
       try {
-        const response = await fetch('https://1ab1-2800-98-1116-780-f9ba-be07-9e81-945e.ngrok-free.app/ParentCategoriesNav');
+        const response = await fetch('https://1ab1-2800-98-1116-780-f9ba-be07-9e81-945e.ngrok-free.app/parentCategoriesNav');
         if (response.ok) {
           const data = await response.json();
           setParentCategories(data);
